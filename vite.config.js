dc7092a7b94efc6aee3367276df951b69b1a9728
Viraj701 <choudhari.viraj@gmail.com>
2026-03-10 22:06:32 +0530

    Set base path for Mahagenco dashboard

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Mahagenco-dashboard/',   // ← ADD THIS LINE
   build: {
     outDir: 'dist',
     sourcemap: false,
